2f85cbf887cb7fc79a6f2668e9ef35a71abde134
TieuLy <yahiko1921@gmail.com>
2023-11-20 15:26:14 +0100

    Bout de CSS inutile enlever

--- frontend/src/pages/Livres/Livres.jsx
+++ frontend/src/pages/Livres/Livres.jsx
@@ -1,12 +1,12 @@
-// Home.jsx
+// Livres.jsx
 
 import React from 'react';
 import { CardBook } from '../../components/atoms';
+import './Livres.css'; 
 
 const Livres = ({ books }) => {
   return (
     <div>
-      <h1>Bienvenue sur la page d'accueil</h1>
       <div className="card-container">
         {books.map((book, index) => (
           <CardBook key={index} title={book.title} price={book.price} image={book.image} />
