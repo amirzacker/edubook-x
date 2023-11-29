3bb1460a328890d3780af55aeb4cab93c6a7120f
Soulaima11 <soulaymatrabelsi84@gmail.com>
2023-11-29 21:31:15 +0100

    mis a jour Categories

--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -1,7 +1,11 @@
 import styled from "styled-components";
-import { categories } from "../toolkit/data";
+
 import { mobile } from "../toolkit/responsive";
 import CategoryItem from "./CategoryItem";
+import loi from "../image/loi.jpg";
+import lavie from "../image/lavie.jpg";
+import nature from "../image/nature.jpg";
+import sport from "../image/sport.jpg";
 
 const Container = styled.div`
   display: flex;
@@ -9,6 +13,33 @@ const Container = styled.div`
   justify-content: space-between;
   ${mobile({ padding: "0px", flexDirection: "column" })}
 `;
+
+ const categories = [
+  {
+    id: 1,
+    img: loi ,
+    title: "NATURE HUMAINE",
+    cat: "loi",
+  },
+  {
+    id: 2,
+    img: lavie,
+    title: "La Vie De Chateau",
+    cat: "lavie",
+  },
+  {
+    id: 3,
+    img: nature,
+    title: " LA NATURE",
+    cat: "nature",
+  },
+    {
+    id: 4,
+    img: sport,
+    title: "SPORT",
+    cat: "sport",
+  },
+];
 
 const Categories = () => {
   return (
